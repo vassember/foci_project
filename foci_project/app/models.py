75325763bc7ce3fa95a0884c44e98e2141e4043a
vassember <vassember@hotmail.com>
2011-08-17 19:55:29 +0200

    uj modell admin

---
+++ foci_project/app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Csapat(models.Model):
+	name = models.CharField(max_length=100)
+	pont = models.IntegerField()
+	def __unicode__(self):
+		return self.name
+
+class Meccs(models.Model):
+	csapat1 = models.ForeignKey(Csapat)	
+	csapat2 = models.ForeignKey(Csapat,related_name='csapat2')
+	date = models.DateTimeField()
+	eredmeny = models.IntegerField()
+	
