8bd0338a4fc3e44c6897846de2b603b16dd21033
vassember <vassember@hotmail.com>
2011-08-19 12:03:12 +0200

    bajnoksag lista order_by

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 from app.models import Meccs,Csapat,Eredmeny
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.db.models import Sum
 # Create your views here.
 
 def index(request):
 	m = Meccs.objects.all()
 	er = Eredmeny.objects.all()
-	return render_to_response('index.html', {'er':er,'m':m},
-		context_instance=RequestContext(request))
-
-
+	cs=Csapat.objects.all()
+	szum = Eredmeny.objects.values('csapat__name').annotate(dsum=Sum('pont')).order_by('-dsum')
+	return render_to_response('index.html', {'szum':szum,'er':er,'m':m,'cs':cs},
+		context_instance=RequestContext(request))	
+	
